70b60871d5759313f756a5078748bffda0172dc7
varunchitre15 <varun.chitre15@gmail.com>
2013-11-15 02:05:14 -0700

    My new build script

--- build.sh
+++ build.sh
@@ -1,2 +1,17 @@
-make ARCH=arm CROSS_COMPILE=~/toolchains/arm-eabi-linaro-4.6.2/bin/arm-eabi- sa77_defconfig
-make ARCH=arm CROSS_COMPILE=~/toolchains/arm-eabi-linaro-4.6.2/bin/arm-eabi- -j8
+TOOLCHAIN="/home/varunchitre15/toolchains/arm-eabi-linaro-4.6.2/bin/arm-eabi"
+MODULES_DIR="/home/varunchitre15/kernel/kernel/modules"
+KERNEL_DIR="/home/varunchitre15/kernel/kernel"
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN- sa77_defconfig
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN- -j8
+if [ -a $KERNEL_DIR/arch/arm/boot/zImage ];
+then
+echo "Copying modules"
+find -name '*.ko' -exec cp -av {} $MODULES_DIR/ \;
+cd $MODULES_DIR
+echo "Stripping modules for size"
+for m in $(find . | grep .ko | grep './')
+do echo $m
+$TOOLCHAIN-strip --strip-unneeded $m
+done
+else
+echo "Compilation failed! Fix the errors!"
