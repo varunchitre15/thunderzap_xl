33c9ba01cd71b128078cca1671fdcd2bbb800ae8
varunchitre15 <varun.chitre15@gmail.com>
2014-01-10 01:02:47 -0700

    Initial bringup: PM8921-Charger Control 1.0
    Introducing charging current control for pm8921 charger chip
    Total controls: 2
    Main switch: /sys/kernel/charger_control/enabled
    Custom current reader: /sys/kernel/charger_control/custom_current_usb
    
    Signed-off-by: varunchitre15 <varun.chitre15@gmail.com>
    
    Conflicts:
    	arch/arm/configs/sa77_defconfig
    	arch/arm/mach-msm/Kconfig
    	arch/arm/mach-msm/Makefile
    	drivers/power/pm8921-charger.c

---
+++ include/linux/pm8921_chg_ctrl.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright © 2014, Varun Chitre "varun.chitre15" <varun.chitre15@gmail.com>
+ *
+ * Kernel header for pm8921-charger Control driver
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+ 
+#ifndef _LINUX_PM8921_CHG_CTRL_H
+#define _LINUX_PM8921_CHG_CTRL_H
+
+extern int mswitch;
+extern int usb_curr_val;
+extern int reg_curr;
+
+#endif
