9cbb565a0046ca3053c508be77277f7925e9f8c9
Varun Chitre <varun.chitre15@gmail.com>
2014-01-20 15:44:52 +0100

    build-script: add zImage check

--- build.sh
+++ build.sh
@@ -3,7 +3,10 @@ TOOLCHAIN="/home/varunchitre15/toolchains/arm-eabi-linaro-4.6.2/bin/arm-eabi"
 MODULES_DIR="../modules"
 ZIMAGE="/home/varunchitre15/kernel/kernel/arch/arm/boot/zImage"
 KERNEL_DIR="/home/varunchitre15/kernel/kernel"
+if [ -a $KERNEL_DIR/arch/arm/boot/zImage ];
+then
 rm $ZIMAGE
+fi
 make ARCH=arm CROSS_COMPILE=$TOOLCHAIN- sa77_defconfig
 make ARCH=arm CROSS_COMPILE=$TOOLCHAIN- -j8
 if [ -a $KERNEL_DIR/arch/arm/boot/zImage ];
