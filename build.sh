ef6f084dbd5d76648218c0ab031240b148469b18
Varun Chitre <varun.chitre15@gmail.com>
2014-02-27 14:02:13 +0100

    Cleanup
    Conflicts:
    	Makefile

--- build.sh
+++ build.sh
@@ -13,10 +13,12 @@ make ARCH=arm CROSS_COMPILE=$TOOLCHAIN- -j8
 if [ -a $KERNEL_DIR/arch/arm/boot/zImage ];
 then
 echo "Copying modules"
+rm $MODULES_DIR/*
 find . -name '*.ko' -exec cp {} $MODULES_DIR/ \;
 cd $MODULES_DIR
 echo "Stripping modules for size"
 $TOOLCHAIN-strip --strip-unneeded *.ko
+zip -9 modules *
 cd $KERNEL_DIR
 else
 echo "Compilation failed! Fix the errors!"
