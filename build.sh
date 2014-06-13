e2e00238847cdcf8d3f243385ed810861f45e227
Varun Chitre <varun.chitre15@gmail.com>
2014-06-13 10:29:04 +0400

    Add build scripts

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+TOOLCHAIN="/home/varunchitre15/toolchains/arm-eabi-linaro-4.6.2/bin/arm-eabi"
+MODULES_DIR="../modules"
+ZIMAGE="/home/varunchitre15/kernel/kernel/arch/arm/boot/zImage"
+KERNEL_DIR="/home/varunchitre15/kernel/kernel"
+if [ -a $KERNEL_DIR/arch/arm/boot/zImage ];
+then
+rm $ZIMAGE
+rm $MODULES_DIR/*
+fi
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN- sa77_defconfig
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN- -j8
+if [ -a $KERNEL_DIR/arch/arm/boot/zImage ];
+then
+echo "Copying modules"
+rm $MODULES_DIR/*
+find . -name '*.ko' -exec cp {} $MODULES_DIR/ \;
+cd $MODULES_DIR
+echo "Stripping modules for size"
+$TOOLCHAIN-strip --strip-unneeded *.ko
+zip -9 modules *
+cd $KERNEL_DIR
+else
+echo "Compilation failed! Fix the errors!"
+fi
