d0305db95dc57d2998b5f3af70f90bf01d7ec187
Varun Chitre <varun.chitre15@gmail.com>
2014-06-13 13:07:23 +0400

    Add boot image creation mechanism
    Signed-off-by: Varun Chitre <varun.chitre15@gmail.com>

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ TOOLCHAIN="/root/toolchains/arm-eabi-linaro-4.6.2/bin/arm-eabi"
 MODULES_DIR="../modules"
 ZIMAGE="/root/tz/arch/arm/boot/zImage"
 KERNEL_DIR="/root/tz"
+MKBOOTIMG="/root/tz/tools/mkbootimg"
+MKBOOTFS="/root/tz/tools/mkbootfs"
 if [ -a $KERNEL_DIR/arch/arm/boot/zImage ];
 then
 rm $ZIMAGE
@@ -20,6 +22,10 @@ echo "Stripping modules for size"
 $TOOLCHAIN-strip --strip-unneeded *.ko
 zip -9 modules *
 cd $KERNEL_DIR
+echo "Creating boot image"
+$MKBOOTFS ramdisk/ > $KERNEL_DIR/ramdisk.cpio
+cat $KERNEL_DIR/ramdisk.cpio | gzip > $KERNEL_DIR/root.fs
+$MKBOOTIMG --kernel $ZIMAGE --ramdisk $KERNEL_DIR/root.fs --cmdline "console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 maxcpus=2" --base 0x80200000 --pagesize 2048 --ramdisk_offset 0x02000000 -o $KERNEL_DIR/boot.img
 else
 echo "Compilation failed! Fix the errors!"
 fi
--- clean.sh
+++ clean.sh
@@ -1,1 +1,5 @@
+KERNEL_DIR="/root/tz"
 make ARCH=arm CROSS_COMPILE=~/toolchains/arm-eabi-linaro-4.6.2/bin/arm-eabi- -j8 clean mrproper
+rm $KERNEL_DIR/ramdisk.cpio
+rm $KERNEL_DIR/root.fs
+rm $KERNEL_DIR/boot.img
