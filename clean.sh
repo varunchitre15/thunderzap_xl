f19430f3ddada4e5dc3e0d838a12b5a206b28ca8
Varun Chitre <varun.chitre15@gmail.com>
2014-06-14 13:13:14 +0400

    force cleaning to avoid errors

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 KERNEL_DIR="/root/tz"
 make ARCH=arm CROSS_COMPILE=~/toolchains/arm-eabi-linaro-4.6.2/bin/arm-eabi- -j8 clean mrproper
-rm $KERNEL_DIR/ramdisk.cpio
-rm $KERNEL_DIR/root.fs
-rm $KERNEL_DIR/boot.img
+rm -rf $KERNEL_DIR/ramdisk.cpio
+rm -rf $KERNEL_DIR/root.fs
+rm -rf $KERNEL_DIR/boot.img
