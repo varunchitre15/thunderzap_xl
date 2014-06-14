ca619edcccfe07bdf7cb73bf4d43c5d68b72b82a
Varun Chitre <varun.chitre15@gmail.com>
2014-06-14 13:17:12 +0400

    Copyright time
    Script going to have major overhault

--- build.sh
+++ build.sh
@@ -1,3 +1,18 @@
+ #
+ # Copyright © 2014, Varun Chitre "varun.chitre15" <varun.chitre15@gmail.com>
+ #
+ # Custom build script
+ #
+ # This software is licensed under the terms of the GNU General Public
+ # License version 2, as published by the Free Software Foundation, and
+ # may be copied, distributed, and modified under those terms.
+ #
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ #
+ #
 #!/bin/bash
 TOOLCHAIN="/root/toolchains/arm-eabi-linaro-4.6.2/bin/arm-eabi"
 MODULES_DIR="../modules"
